51531780daf7bdd1b253231ac0398df615f8ca0e
Renan Nascimento <atomsfrontend@gmail.com>
2025-06-08 15:35:20 -0300

    chore: config next from accept images and urls

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins:
+    process.env.NODE_ENV === "development"
+      ? ["https://e42f-2804-14c-213-468f-5d3d-8cac-85f9-2374.ngrok-free.app"]
+      : [],
+  images: {
+    remotePatterns: [{ hostname: "u9a6wmr3as.ufs.sh" }],
+  },
 };
 
 export default nextConfig;
