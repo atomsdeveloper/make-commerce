347925afa53af1d20d7eaa5de21fc0c446576fa5
Renan Nascimento <atomsfrontend@gmail.com>
2025-06-09 19:29:46 -0300

    fix: public url from ngrok

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins:
     process.env.NODE_ENV === "development"
-      ? ["https://e42f-2804-14c-213-468f-5d3d-8cac-85f9-2374.ngrok-free.app"]
+      ? ["https://d568-2804-14d-783a-8678-a42f-61e2-594b-2573.ngrok-free.app"]
       : [],
   images: {
     remotePatterns: [{ hostname: "u9a6wmr3as.ufs.sh" }],
