58c8bc2d40c7b9d4768102d6dca95de5ce24f301
Renan Nascimento <atomsfrontend@gmail.com>
2025-06-12 23:45:31 -0300

    chore: configure url to accept ngrok

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins:
     process.env.NODE_ENV === "development"
-      ? ["https://d568-2804-14d-783a-8678-a42f-61e2-594b-2573.ngrok-free.app"]
+      ? ["https://4d37-2804-14c-213-468f-ed90-226e-a637-8615.ngrok-free.app"]
       : [],
   images: {
     remotePatterns: [{ hostname: "i.postimg.cc" }],
