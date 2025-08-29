5931f33685d55d4fe058757527afb2336602df49
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-28 22:31:58 -0300

    chore: add todo

--- src/data/get-store-by-slug.ts
+++ src/data/get-store-by-slug.ts
@@ -1,5 +1,6 @@
 import { db } from "../lib/prisma";
 
+// TODO: Create design pattern Repository.
 export const getStoreBySlug = async (slug: string) => {
   const store = await db.store.findUnique({ where: { slug } });
   return store;
