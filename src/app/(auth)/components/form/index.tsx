012bd59a5555bf0fc06e4525c0c6c56810da3345
Renan Nascimento <atomsfrontend@gmail.com>
2025-08-31 01:14:19 -0300

    chore: basic fix

--- src/app/(auth)/components/form/index.tsx
+++ src/app/(auth)/components/form/index.tsx
@@ -13,9 +13,34 @@ import { LogInIcon } from "lucide-react";
 // Next
 import Link from "next/link";
 
+// React
+// import { useActionState } from "react";
+
+// Action
+// import { loginAction } from "@/src/actions/loginAction";
+
 export default function Form() {
+  // TODO: Add form validation and types
+  // const initialState = {
+  //   formState: {
+  //     email: "",
+  //     password: "",
+  //   },
+  //   error: [],
+  //   success: false,
+  // };
+
+  // const { state, action, isPending } = useActionState(
+  //   loginAction,
+  //   initialState
+  // );
+
   return (
-    <form role="form" className="flex flex-col justify-around h-full">
+    <form
+      action={""}
+      role="form"
+      className="flex flex-col justify-around h-full"
+    >
       <CardContent className="h-full flex flex-col mt-1">
         <div className="flex flex-col gap-6">
           {/* TODO: Separate component of input and label, both are equals. */}
@@ -32,7 +57,9 @@ export default function Form() {
               aria-label="Dígite seu e-mail."
               autoComplete="username"
               placeholder="m@example.com"
+              // defaultValue={state.formState.email}
               required
+              //  disabled={isPending}
             />
           </div>
 
@@ -49,6 +76,7 @@ export default function Form() {
               autoComplete="current-password"
               placeholder="Dígite a senha."
               required
+              // disabled={isPending}
             />
           </div>
         </div>
@@ -56,21 +84,23 @@ export default function Form() {
 
       <CardFooter className="flex flex-col  justify-between">
         <Button
+          data-testid="button-element-signin"
           type="submit"
           variant="outline"
           className="w-full bg-stone-950 text-stone-100 mt-2 mb-2 text-sm"
           aria-label="Clique para enviar os dados e entrar na plataforma."
+          // disabled={isPending}
         >
-          Login
+          Entrar
         </Button>
 
         <SignInButton>
           <div
             className="w-full mt-2 mb-2 flex items-center text-sm justify-center border rounded-md px-4 py-2 hover:bg-gray-100"
-            aria-label="Clique para entrar com a conta do google."
+            aria-label="Entrar com o Google."
           >
             <LogInIcon className="mr-2" size={16} />
-            Login with Google
+            Entrar com o Google
           </div>
         </SignInButton>
 
@@ -79,6 +109,7 @@ export default function Form() {
           title="Criar conta totalmente grátis."
           aria-label="Quero cadastrar uma conta grátis."
           className="mt-2 mb-2 text-xs underline italic cursor-pointer hover:text-slate-800"
+          // aria-disabled={isPending}
         >
           Quero cadastrar uma conta grátis!
         </Link>
