e4b89c46d639a212e4ffdf2935f66497153c71f5
Renan Nascimento <atomsfrontend@gmail.com>
2025-09-03 13:45:17 -0300

    chore: rename file

--- src/data/get-store.ts
+++ src/data/get-store.ts
@@ -1,6 +1,8 @@
+// TODO: Create design pattern Repository.
+
+// Database
 import { db } from "../lib/prisma";
 
-// TODO: Create design pattern Repository.
 export const getStoreBySlug = async (slug: string) => {
   const store = await db.store.findUnique({ where: { slug } });
   return store;
