6b6ac0dd8c9b647060932c3a5e92236ff6152c58
Renan Nascimento <atomsfrontend@gmail.com>
2025-10-19 23:29:20 -0300

    chore: remove files useless

--- src/lib/user/queries/publish.ts
+++
@@ -1,9 +0,0 @@
-import { InstanceUserRepository } from "@/src/repositories/user";
-import { UserModel } from "../../../model/user/user-model";
-
-// React
-import { cache } from "react";
-
-export const createdUserCache = cache(async (user: UserModel) => {
-  return await InstanceUserRepository.createUser(user);
-});
