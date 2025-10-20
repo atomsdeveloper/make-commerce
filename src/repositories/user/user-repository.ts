903d2209902f1509cefa2500092c7c05c4ed0ff0
Renan Nascimento <atomsfrontend@gmail.com>
2025-10-19 23:58:12 -0300

    chore: remove files useless

--- src/repositories/user/user-repository.ts
+++
@@ -1,8 +0,0 @@
-// Model
-import { UserModel } from "@/src/model/user/user-model";
-
-// Repository Pattern
-export interface UserRepository {
-  // Mutation
-  createUser(user: UserModel): Promise<UserModel>;
-}
